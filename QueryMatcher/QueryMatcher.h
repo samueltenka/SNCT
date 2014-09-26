cfc333a7738d4229b629de2967fecaebaff50c4d
samueltenka <iguavadon@gmail.com>
2014-09-26 06:07:10 -0700

    Create QueryMatcher.h
    Started writing query matcher header.

---
+++ QueryMatcher/QueryMatcher.h
@@ -0,0 +1,17 @@
+#ifndef QUERY_MATCHER
+#define QUERY_MATCHER
+
+
+
+#include <string>
+using namespace std;
+
+/*****************************************************
+// SENTENCE_MATCH:
+//    searches for sentences containing ``goal''
+//    in large knowledgebase ``giant_text''.
+*****************************************************/
+string sentence_match(string giant_text, string goal);
+
+
+#endif QUERY_MATCHER
