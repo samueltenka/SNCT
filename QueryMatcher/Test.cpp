154ffa42e4224095f70b62a1ca924d900ae010f3
samueltenka <iguavadon@gmail.com>
2014-09-26 13:35:23 -0700

    Update Test.cpp
    Tests passed! (code does what it looks like it should do).
    
    However, "what it looks like it should do" can be improved greatly: as seen in output below, sentences come with unchomped spaces, and some are incomplete (due to use of dots not only as periods but also in abbreviations, etc. . . .)
    
    
    OUTPUT:
    Results for "white shark and tuna":
    ***********************************
    
     Most fish are ectothermic ("cold-blooded"), allowing their body temperatures to
     vary as ambient temperatures change, though some of the large active swimmers l
    ike white shark and tuna can hold a higher core temperature.
    
    Results for "fish is" and "fish are":
    *************************************
    
    A fish is any member of a paraphyletic group of organisms that consist of all gi
    ll-bearing aquatic craniate animals that lack limbs with digits.
    
    ) than of other fish such as ray-finned fish or sharks, so the last common ances
    tor of all fish is also an ancestor to tetrapods.
    
    
    
    A typical fish is ectothermic, has a streamlined body for rapid swimming, extrac
    ts oxygen from water using gills or uses an accessory breathing organ to breathe
     atmospheric oxygen, has two sets of paired fins, usually one or two (rarely thr
    ee) dorsal fins, an anal fin, and a tail fin, has jaws, has skin that is usually
     covered with scales, and lays eggs.
    
     The larval period in oviparous fish is relatively short (usually only several w
    eeks), and larvae rapidly grow and change appearance and structure (a process te
    rmed metamorphosis) to become juveniles.
    
    
    
    In biology --- the term fish is most strictly used to describe any animal with a
     backbone that has gills throughout life and has limbs, if any, in the shape of
    fins.
    
    [77]
    
    In fisheries --- the term fish is used as a collective term, and includes mollus
    ks, crustaceans and any aquatic animal which is harvested.

--- QueryMatcher/Test.cpp
+++ QueryMatcher/Test.cpp
@@ -5,12 +5,28 @@
 #define KNOW_PATH "C:\\Users\\Sam\\Desktop\\SNCT\\fish_knowledgebase.txt"
 
 
+#define PRINT_RESULTS_OF(query) { \
+   cout << "" << endl; \
+   list<string> queries = {query}; \
+   list<string> results = find_relevant_sentences(knowledgebase, queries); \
+   for(list<string>::const_iterator res_ptr = results.begin(); res_ptr != results.end(); ++res_ptr) { \
+      cout << (*res_ptr) << endl << endl; \
+   } \
+}
+
+
 int main() {
 	string knowledgebase = read_string_from(KNOW_PATH);
-	cout << knowledgebase << endl;
+	/*cout << knowledgebase << endl;*/
 
-  // FILL IN TEST
+   cout << "Results for \"white shark and tuna\":" << endl;
+   cout << "***********************************" << endl;
+   PRINT_RESULTS_OF("white shark and tuna");
 
-	cout << "Tada!" << endl;
-	char l;  cin >> l;
+   cout << "Results for \"fish is\" and \"fish are\":" << endl;
+   cout << "*************************************" << endl;
+   PRINT_RESULTS_OF("fish is", "fish are");
+   
+   cout << "Tada!" << endl;
+   char l;  cin >> l;
 }
