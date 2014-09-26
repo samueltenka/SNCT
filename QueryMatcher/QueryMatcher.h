bde0389c18d487b2bffc4d7ef6e80057328ad4d5
samueltenka <iguavadon@gmail.com>
2014-09-26 06:51:27 -0700

    Update QueryMatcher.h
    Refactored search process into two pieces: finding indices, and from there generating consensus.
    Hence, we have many from one, then one from the many.
    This local detail parallels the global structure of our program.

--- QueryMatcher/QueryMatcher.h
+++ QueryMatcher/QueryMatcher.h
@@ -4,14 +4,23 @@
 
 
 #include <string>
+#include <list>
 using namespace std;
 
-/*****************************************************
-// SENTENCE_MATCH:
-//    searches for sentences containing ``goal''
-//    in large knowledgebase ``giant_text''.
-*****************************************************/
-string sentence_match(string giant_text, string goal);
+
+/*****************************************************************************
+// FIND_RELEVANT_SENTENCES:
+//    searches for sentences containing an element of ``queries'' in 
+//    large ``knowledgebase''.
+*****************************************************************************/
+list<string> find_relevant_sentences(string knowledgebase, list<string> queries);
+
+/************************************************************
+// GENERATE_CONSENSUS:
+//    based on (hopefully related) ``relevant_sentences'',
+//    generates coherent summary.
+************************************************************/
+string generate_consensus(list<string> relevant_sentences);
 
 
 #endif QUERY_MATCHER
