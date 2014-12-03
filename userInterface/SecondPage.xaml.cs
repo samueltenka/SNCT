6e5e79a333c66a080b73766f69dd8ce0afc0160f
neek <nialfe@umich.edu>
2014-12-03 13:02:29 -0500

    ui files

---
+++ userInterface/SecondPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace SnickIt_Test1
+{
+    public partial class SecondPage : PhoneApplicationPage
+    {
+        public SecondPage()
+        {
+            InitializeComponent();
+        }
+
+        private void HyperlinkButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void HyperlinkButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/ThirdPage.xaml", UriKind.Relative));
+        }
+    }
+}
