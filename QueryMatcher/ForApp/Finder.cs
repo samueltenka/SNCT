c94012cabc5ccd27a6e54a08400653e5b4c648ea
samueltenka <iguavadon@gmail.com>
2014-12-08 13:11:40 -0500

    Update Finder.cs

--- QueryMatcher/ForApp/Finder.cs
+++ QueryMatcher/ForApp/Finder.cs
@@ -9,7 +9,7 @@ namespace SNCT
 {
     public class Finder
     {
-        public static SortedDictionary<String, double> answer(String text, String[] queries, int num)
+        async public static Task<SortedDictionary<String, double>> answer(String text, String[] queries, int num)
         {
             HashSet<String> query_content_words = new HashSet<String>();
             foreach(String q in queries) {
@@ -18,7 +18,7 @@ namespace SNCT
                 }
             }
 
-            PhraseGraph PG = new PhraseGraph(text, queries[0], query_content_words);
+            PhraseGraph PG = await PhraseGraph.build_PG(text, queries[0], query_content_words);
             for(int i = 0; i < 1000; ++i)
             {
                 PG.step(0.01);
